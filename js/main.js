b3e3f5149faf463e6ccc25ad8b73f95e507776a1
Vlad <shevyakov-vlad@mail.ru>
2019-07-10 10:54:10 +0400

    edit

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ engine.addGround(new Ground('./image/ground_1.png', groundWidth, groundHeight, s
 engine.addGround(new Ground('./image/ground_2.png', groundWidth, groundHeight, scale));
 engine.addGround(new Ground('./image/ground_3.png', groundWidth, groundHeight, scale));
 
-engine.player = new Player('./image/run.png', 20, engine.canvas.height * 0.72, scale);
+engine.player = new Player('./image/idle.png', 20, engine.canvas.height * 0.72, scale);
 
 engine.update = (dt) => {
 	if ((Math.floor(-engine.camera.x / groundWidth / scale)) % 3 == 0) {
