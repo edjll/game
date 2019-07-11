3e5430a6ef0955ed9947bd7cebfd797c534bacbf
Frank-Woods <49346787+Frank-Woods@users.noreply.github.com>
2019-07-11 19:27:53 +0400

    test

--- js/settings.js
+++ js/settings.js
@@ -1,28 +1,48 @@
 let play 		= document.getElementById('play'),
 	help  		= document.getElementById('help'),
-	helpOn  	= document.getElementById('helpOn'),
+	helpO  	= document.getElementById('helpOff'),
 	loginForm 	= document.getElementById('login'),
 	start 		= document.getElementById('start'),
 	cancel 		= document.getElementById('cancel'),
 	nickname 	= document.getElementById('nickname');
 	login 		= document.getElementById('login');
+	main		= document.getElementById('main');
+	flag		= 0;
 
 play.onclick = () => {
+	if (flag == -1) {
+		play.className = 'container_button';
+		help.className = 'container_button';
+		helpO.id = 'helpOff';
+	}
+	flag = 1;
 	loginForm.id = 'loginOn';
-	play.className += ' out';
-	help.className += ' out';
+	play.className += ' left_out';
+	help.className += ' left_out';
 }
 
 help.onclick = () => {
-	helpOn.className = 'helpOn'
+	if (flag == 1) {
+		loginForm.id = 'login';
+		play.className = 'container_button';
+		help.className = 'container_button';
+	}
+	flag = -1;
+	helpO.id = 'helpOn'
+	play.className += ' right_out';
+	help.className += ' right_out';
 }
 
 cancel.onclick = () => {
-	helpOn.className = '';
+	flag == 0;
+	play.className = 'container_button';
+	help.className = 'container_button';
+	helpO.id = 'helpOff';
 }
 
 start.onclick = () => {
-	login.className +=' rotate';	
+	login.className += ' rotate_right';
+	main.className += ' rotate_left'	
 	if (nickname.value.length) {
 		localStorage.setItem('nickname', nickname.value);
 		window.location = './game.html';
