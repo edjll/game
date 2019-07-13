8c08faaf3658b58ef0405fa02022f521e6b86a32
Frank-Woods <49346787+Frank-Woods@users.noreply.github.com>
2019-07-13 16:58:02 +0400

    test

--- js/settings.js
+++ js/settings.js
@@ -3,6 +3,7 @@ let
 	loginForm 	= document.getElementById('login'),
 	start 		= document.getElementById('start'),
 	nickname 	= document.getElementById('nickname');
+	end			= document.getElementById('end')
 
 
 start.onclick = () => {
@@ -24,3 +25,35 @@ start.onclick = () => {
 let soundTrack = document.getElementById('soundTrack');
 soundTrack.play();
 soundTrack.volume = 0.8;
+
+if (!this.game) {
+	end.id += 'on'; 
+	function sortLocalStorage(){
+    if(localStorage.length > 0){
+          var localStorageArray = [];
+          for (i = 0; i < localStorage.length; i++){
+              localStorageArray[i] = {
+                  key:    localStorage.key(i),
+                  value: localStorage.getItem(localStorage.key(i)),
+              	}
+        	}
+    	}
+
+    	function sortArrayObject(a, b) {
+        	if (a['key'] > b['key']) {
+            	return 1;
+        	}
+        	if (a['key'] < b['key']) {
+           	 	return -1;
+        	}
+    	}
+
+   	 	sortLocalStorageArray = localStorageArray.sort(sortArrayObject);
+
+   	 	for (i = 0; i<localStorage.length || i<10; i++){
+    		key = localStorage.key(i);
+    		document.getElementsByClassName('res')[0].innerHTML += key + mass_elemets[i];
+		}
+	}
+
+}
