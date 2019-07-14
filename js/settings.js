9f955ca8e9afdd15e7303b0cc5f5a07798112b90
Frank-Woods <49346787+Frank-Woods@users.noreply.github.com>
2019-07-14 15:11:35 +0400

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -41,39 +41,6 @@ let soundTrack = document.getElementById('soundTrack');
 soundTrack.play();
 soundTrack.volume = 0.8;
 
-/*if (!this.game) {
-	end.id += 'on'; 
-	function sortLocalStorage(){
-    if(localStorage.length > 0){
-          var localStorageArray = [];
-          for (i = 0; i < localStorage.length; i++){
-              localStorageArray[i] = {
-                  key:    localStorage.key(i),
-                  value: localStorage.getItem(localStorage.key(i)),
-              	}
-        	}
-    	}
-
-    	function sortArrayObject(a, b) {
-        	if (a['key'] > b['key']) {
-            	return 1;
-        	}
-        	if (a['key'] < b['key']) {
-           	 	return -1;
-        	}
-    	}
-
-   	 	sortLocalStorageArray = localStorageArray.sort(sortArrayObject);
-
-   	 	for (i = 0; i<localStorage.length || i<10; i++){
-    		key = localStorage.key(i);
-    		document.getElementsByClassName('res')[0].innerHTML += key + mass_elemets[i];
-		}
-	}
-
-}*/
-
-
 function sortLocalStorage() {
 	if(localStorage.length > 0) {
         let localStorageArray 	  = [];
