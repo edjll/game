4c393c7b0d900b69523253d292bf0b5f672df165
Vlad <shevyakov-vlad@mail.ru>
2019-07-15 13:37:01 +0400

    edit

--- js/engine.js
+++ js/engine.js
@@ -74,7 +74,7 @@ class Engine {
 			this.gameTime = realTime - this.timePause;
 
 			if (this.gameTime > this.botTime + 5000) {
-				if (this.bots.bot.length < 1) {
+				if (this.bots.bot.length < 5 && this.bots.bot.length < Math.floor(-this.camera.x / this.canvas.width)) {
 					this.bots.addBot(-this.camera.x + Math.random() * (this.canvas.width - 200 * this.player.scale) + 100* this.player.scale);
 				}
 				this.botTime = this.gameTime;
